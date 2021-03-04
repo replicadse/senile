89932a4c03e8454d6d344a6361622ea96dfa2e0c
dejhhw0l <heiko-alexander.weber@jungheinrich.de>
2021-03-04 16:56:30 +0100

    multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    str,
+    thread,
+};
 
+use crossbeam::{
+    channel::{
+        unbounded,
+        Sender,
+    },
+    sync::WaitGroup,
+};
 use futures::executor::block_on;
 use walkdir::WalkDir;
 mod args;
@@ -27,30 +38,27 @@ use crate::error::ParserError;
 
 // TODO!(2, haw): Add multithreading support for reading the files.
 
-
 struct ContentParserParams {
     file: String,
-    todo_start_str_combined: String
+    todo_start_str_combined: String,
 }
-struct ContentParserState {
-}
+struct ContentParserState {}
 struct ContentParser<'s> {
-    cursor: &'s mut Cursor::<Vec<u8>>,
+    cursor: &'s mut Cursor<Vec<u8>>,
     info: ContentParserParams,
     #[allow(dead_code)]
     state: ContentParserState,
 }
 impl<'s> ContentParser<'s> {
-    pub fn new(cursor: &'s mut Cursor::<Vec<u8>>, info: ContentParserParams) -> Self {
+    pub fn new(cursor: &'s mut Cursor<Vec<u8>>, info: ContentParserParams) -> Self {
         Self {
             cursor,
             info,
-            state: ContentParserState {
-            }
+            state: ContentParserState {},
         }
     }
 
-    pub async fn parse(&mut self) -> Result<Vec<ToDoItem>, Box<dyn Error>> {
+    pub fn parse(&mut self) -> Result<Vec<ToDoItem>, Box<dyn Error>> {
         let mut todos = Vec::<ToDoItem>::new();
         let mut buf = String::new();
         let mut line = 0u32;
@@ -65,7 +73,7 @@ impl<'s> ContentParser<'s> {
             }
 
             buf = buf.trim().to_owned();
-            if let Ok(res) = self.parse_buf(line, &buf).await {
+            if let Ok(res) = self.parse_buf(line, &buf) {
                 match res {
                     | Some(v) => {
                         todos.push(v);
@@ -80,11 +88,7 @@ impl<'s> ContentParser<'s> {
         Ok(todos)
     }
 
-    async fn parse_buf(
-        &mut self,
-        line: u32,
-        buf: &str,
-    ) -> Result<Option<ToDoItem>, Box<dyn Error>> {
+    fn parse_buf(&mut self, line: u32, buf: &str) -> Result<Option<ToDoItem>, Box<dyn Error>> {
         if let Some(start_idx) = buf.find(&self.info.todo_start_str_combined) {
             let p_error = ParserError::new(&format!("failed to parse line {} in file {}", line, self.info.file));
             let sub_buf = &buf[start_idx..];
@@ -113,7 +117,6 @@ impl<'s> ContentParser<'s> {
     }
 }
 
-
 #[derive(Debug, Serialize, Deserialize)]
 struct ToDoItem {
     priority: String,
@@ -129,39 +132,77 @@ const TODO_PARAMS_SEPARATOR: &'static str = ",";
 const MIN_TODO_LEN: usize =
     TODO_START_STR.len() + TODO_PARAMS_PARANTHESES_START.len() + TODO_PARAMS_PARANTHESES_END.len() + 2;
 
-
-async fn collect(path: &str, filter: &str) -> Result<(), Box<dyn Error>> {
-    let mut all_todos = HashMap::<String, Vec<ToDoItem>>::new();
-    let mut todo_start_str_combined = String::new();
-    todo_start_str_combined.push_str(TODO_START_STR);
-    todo_start_str_combined.push_str(TODO_PARAMS_PARANTHESES_START);
-    let matcher = Regex::new(filter)?;
-    for entry in WalkDir::new(path)
-        .into_iter()
-        .filter_map(Result::ok)
-        .filter(|e| !e.file_type().is_dir())
-    {
-        let entry_path = entry.into_path();
-        let entry_path_str = entry_path.to_str().unwrap();
-        if !matcher.is_match(entry_path_str)? {
-            continue;
-        }
-        let file_content = fs::read(entry_path_str)?;
-        let mut cursor = Cursor::new(file_content);
-
-        let mut parser = ContentParser::new(&mut cursor, ContentParserParams {
-            file: entry_path_str.to_owned(),
-            todo_start_str_combined: todo_start_str_combined.clone(),
-        });
-
-        if let Ok(todos) = parser.parse().await {
-            // sort in todos
-            for v in todos {
-                all_todos.entry(v.priority.to_owned()).or_insert(Vec::new());
-                all_todos.get_mut(&v.priority.to_owned()).unwrap().push(v);
-            }
-        }
+struct Crawler<'s> {
+    matcher: &'s Regex,
+}
+impl<'s> Crawler<'s> {
+    pub fn new(matcher: &'s Regex) -> Self {
+        Self { matcher }
     }
+
+    pub fn crawl(&self, path: &str, sender: Sender<String>) -> Result<(), Box<dyn Error>> {
+        for entry in WalkDir::new(path)
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(|e| !e.file_type().is_dir())
+        {
+            let entry_path = entry.into_path();
+            let entry_path_str = entry_path.to_str().unwrap();
+            if !self.matcher.is_match(entry_path_str)? {
+                continue;
+            }
+            sender.send(entry_path_str.to_owned())?;
+        }
+        Ok(())
+    }
+}
+
+fn collect(path: String, filter: String) -> Result<(), Box<dyn Error>> {
+    let (sender_crawler, receiver_crawler) = unbounded();
+
+    // fire and forget thread
+    let crawler_thread = thread::spawn(move || {
+        let matcher = Regex::new(&filter).unwrap();
+        let crawler = Crawler::new(&matcher);
+        crawler.crawl(&path, sender_crawler).unwrap();
+    });
+
+    let wg = WaitGroup::new();
+    let (sender_parser, receiver_parser) = unbounded();
+    for s in receiver_crawler.iter() {
+        let thread_wg = wg.clone();
+        let thread_sender = sender_parser.clone();
+        thread::spawn(move || {
+            let dothis = move || -> Result<(), Box<dyn Error>> {
+                let mut todo_start_str_combined = String::new();
+                todo_start_str_combined.push_str(TODO_START_STR);
+                todo_start_str_combined.push_str(TODO_PARAMS_PARANTHESES_START);
+                let content = fs::read(&s)?;
+                let mut cursor = Cursor::new(content);
+                let mut parser = ContentParser::new(&mut cursor, ContentParserParams {
+                    file: s,
+                    todo_start_str_combined,
+                });
+                let todos = parser.parse()?;
+                for t in todos {
+                    thread_sender.send(t)?;
+                }
+                Ok(())
+            };
+            dothis().unwrap_or_default();
+            drop(thread_wg);
+        });
+    }
+    crawler_thread.join().expect("the crawler thread has panicked");
+
+    let mut all_todos = HashMap::<String, Vec<ToDoItem>>::new();
+    drop(sender_parser);
+    for todo in receiver_parser {
+        all_todos.entry(todo.priority.to_owned()).or_insert(Vec::new());
+        all_todos.get_mut(&todo.priority).unwrap().push(todo);
+    }
+    wg.wait();
+
     let output = serde_json::to_vec(&all_todos)?;
     let stdout = std::io::stdout();
     let mut stdout_lock = stdout.lock();
@@ -174,7 +215,7 @@ async fn main_async() -> Result<(), Box<dyn Error>> {
     let args = ClapArgumentLoader::load().await?;
     match args.command {
         | Command::Collect { path, filter } => {
-            collect(&path, &filter).await?;
+            collect(path, filter)?;
             Ok(())
         },
     }
