ccbd39fa3f39e66029ed4cdf9c542faf1132fad2
replicadse <replicadse@users.noreply.github.com>
2021-03-08 21:59:12 +0000

    deploy: 075f51007b29ae985485f95e24cbe2fe0c6745ef

--- todos.py
+++ todos.py
@@ -21,12 +21,12 @@ for d in data:
     data_prio[d['priority']].append(d)
 
 def print_todo(d):
-    print('- prio: {}\\'.format(d['priority']))
-    print('  assignee: {}\\'.format(d['assignee']))
-    print('  body: {}\\'.format(d['body']))
+    print('- prio: `{}`\\'.format(d['priority']))
+    print('  assignee: `{}`\\'.format(d['assignee']))
+    print('  body: `{}`\\'.format(d['body']))
     print('  context: \n\t```\n\t{}\n\t```'.format('\n\t'.join([l.strip().strip('\n') for l in d['context']])))
-    print('  file: {}\\'.format(d['file']))
-    print('  line: {}\\'.format(d['line']))
+    print('  file: `{}`\\'.format(d['file']))
+    print('  line: `{}`\\'.format(d['line']))
 
 for assignee in data_assignee.keys():
     print('### {}'.format(assignee))
